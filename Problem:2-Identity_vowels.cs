7d2ede6a13e166dca3a3289d77fc1b911569805e
jobin-K-98 <122489680+jobin-K-98@users.noreply.github.com>
2023-05-21 01:50:31 -0400

    Update Problem:2-Identity_vowels.cs
    Added some modifications to the code according to the rules mentioned in moodle.
    
    Added a do while loop to give the user access to continue or quit the program.
    
    the program stores all the vowels in an array and checks whether the entered alphabet exist in the array. if it exists in the array the bool variable flag is set to true. According to the value of bool the output is displayed.
    
    The program uses array and for loop rather than using if/else condition or switch statements for reducing the number of line of code.

--- Problem:2-Identity_vowels.cs
+++ Problem:2-Identity_vowels.cs
@@ -1,42 +1,43 @@
 //Applied activity 1 : Problem 2 - Identify Vowels
 
-using System;
+//variables required for the program
+bool flag = false;
+string choice;
 
-internal class Program
+// do while loop for the user choice to continue checking vowels
+do
 {
-    private static void Main(string[] args)
+    // Getting user input
+    Console.WriteLine("Enter an Alphabet: ");
+    string userInput = Console.ReadLine();
+
+    string alphabet = userInput.ToLower();
+
+    //Storing the vowels
+    string[] vowels = { "a", "e", "i", "o", "u" };
+
+    // looping through the vowels array
+    for (int i = 0; i < vowels.Length; i++)
     {
-        // Getting user input
-        Console.WriteLine("Enter an Alphabet: ");
-        string userInput = Console.ReadLine();
-
-        string alphabet = userInput.ToLower();
-
-        bool flag = false;
-
-        //Storing the vowels
-        string[] vowels = { "a", "e", "i", "o", "u" };
-
-        // looping through the vowels array
-        for(int i = 0; i < vowels.Length; i++)
+        if (alphabet == vowels[i])
         {
-            if (alphabet == vowels[i])
-            {
-                flag = true;
-                break;
-            }
+            flag = true;
+            break;
         }
+    }
 
-        // Checking if the alphabet is vowel or not
-        if(flag)
-        {
-            Console.WriteLine("Vowel");
-        }
-        else
-        {
-            Console.WriteLine("Not a vowel!");
-        }
-        
+    // Checking if the alphabet is vowel or not
+    if (flag)
+    {
+        Console.WriteLine("Vowel");
+    }
+    else
+    {
+        Console.WriteLine("Not a vowel!");
+    }
 
-}
-}
+    Console.WriteLine("Do you want to continue (y/n): ");
+
+    choice = Console.ReadLine();
+
+} while (choice == "y");
